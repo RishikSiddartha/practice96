ba2dda0c71729725073a9d20c9f93dbd732fcce4
RishikSiddartha <RishikSiddartha2005@gmail.com>
2024-12-10 15:25:42 +0530

    hello

---
+++ celsius1.java
@@ -0,0 +1,8 @@
+public class celsius1  
+ {  
+   public static void main (String args[])  
+    { float Fahrenheit, Celsius;  
+          Fahrenheit = 43;  
+          Celsius  = ((Fahrenheit-32)*5)/9;  
+          System.out.println("Temperature in celsius is: "+Celsius);  
+    }}  
